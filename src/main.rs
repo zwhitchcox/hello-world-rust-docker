131d6d5369b04ecfbaf30a30512eac90a44af93a
Zane Hitchcox <zwhitchcox@gmail.com>
2020-07-14 08:55:39 -0400

    0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,5 @@ async fn main() {
     // Match any request and return hello world!
     let routes = warp::any().map(|| "Hello, World!");
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
